323d1f718122426a4832bd1085e595dc5c084eef
황창현 <changhyeon.h@kakao.com>
2023-05-21 16:10:47 +0900

    Feat: springboot fetch test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [msg, setMsg] = useState({});
+  useEffect( () => {
+     fetch("/api/test").then( (res) => {return res.json();}).then((data) => {setMsg(data); console.log("완료")});
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {msg.id}
+      {msg.name}
+      {msg.birthday}
     </div>
   );
 }
